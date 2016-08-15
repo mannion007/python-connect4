13d06ef5739493c65a220ee0c2d26c12d8b1bc72
James Mannion <mannion007@gmail.com>
2016-08-15 17:11:03 +0100

    Win checking implemented

--- connect.py
+++ connect.py
@@ -13,7 +13,6 @@ game_over = False
 board = []
 
 def setup_game():
-	game_over = False
 	for row in range(board_height):
 		board.append([empty_char] * board_width)
 
@@ -30,6 +29,27 @@ def board_full():
 			return False
 	return True
 
+def player_move():
+	valid = False
+	message = 'Player, choose a column...'
+
+	while not valid:
+		col = raw_input(message)
+		valid = move_valid(col)
+		message = 'Player, that choice is not valid. Please choose another column...'
+
+	move(int(col), player_char)
+
+def computer_move():
+	valid = False
+
+	while not valid:
+		col = randint(0, board_width - 1)
+		valid = move_valid(col)
+
+	print('Computer chose column %s' % str(col))
+	move(col, computer_char)
+
 def move(col, char):
 	if not col_full(col):
 		for row in range(len(board)-1, -1, -1):
@@ -38,33 +58,39 @@ def move(col, char):
 				player_turn = False
 				break
 
-def player_move():
-	valid = False
-	message = 'Player, choose a column...'
+def move_valid(col):
+	try:
+		col = int(col)
+	except 	ValueError:
+		return False
 
-	while not valid:
-		col = int(raw_input(message))
-		valid = col_valid(col)
-		message = 'Player, that choice is not valid. Please choose another column...'
-
-	move(col, player_char)
-
-def computer_move():
-	valid = False
-
-	while not valid:
-		col = randint(0, board_width - 1)
-		valid = col_valid(col)
-
-	print('Computer chose column %s' % str(col))
-	move(col, computer_char)
-
-def col_valid(col):
 	if col < 0 or col > board_width - 1:
 		return False
 	elif col_full(col):
 		return False
 	return True
+
+def game_won(char):
+	for col in range(board_width):
+		for row in range(board_height):
+			if board[row][col] == char:
+				# Horizontal line
+				if col + 4 <= board_width:
+					if board[row][col + 1] == char and board[row][col + 2] == char and board[row][col + 3] == char:
+						return True
+				# Vertical line
+				if row + 4 <= board_height:
+					if board[row + 1][col] == char and board[row + 2][col] == char and board[row + 3][col] == char:
+						return True
+				# Diagonal down
+				if col + 4 <= board_width and row + 4 <= board_height:
+					if board[row + 1][col + 1] == char and board[row + 2][col + 2] == char and board[row + 3][col + 3] == char:
+						return True
+				# Diagonal up
+				if col - 4 >= 0 and row - 4 >= 0:
+					if board[row - 1][col - 1] == char and board[row - 2][col - 2] == char and board[row - 3][col - 3] == char:
+						return True
+	return False
 
 def game_over():
 	if board_full():
@@ -72,7 +98,7 @@ def game_over():
 
 setup_game()
 
-while not game_over():
+while not game_won(player_char) and not game_over():
 	if player_turn:
 		player_move()
 		player_turn = False
